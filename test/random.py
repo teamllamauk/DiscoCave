df9aee18d2e6f04b03ec4d285367e1e620b23368
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-14 09:58:48 +0000

    Update random.py

--- test/random.py
+++ test/random.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
 
+from random import *
+ 
+audio_rand = randint(1, 50)    # Pick a random number between 1 and 50.
+print(audio_rand)
+
+if audio_rand > 35 and audio_rand < 41 :
+    print("Vegimals")
+else:
+    print("octo")    
