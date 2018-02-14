8fc9e62a610bec1f2ee33787fe42901f7d487173
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-14 10:52:00 +0000

    Update buttons.py

--- test/buttons.py
+++ test/buttons.py
@@ -1,1 +1,75 @@
+#!/usr/bin/env python3
 
+import RPi.GPIO as rGPIO
+
+rGPIO.setmode(GPIO.BCM)
+rGPIO.setwarnings(False)
+
+btn_red_pin = 26
+btn_green_pin = 19
+btn_blue_pin = 13
+btn_orange_pin = 6
+btn_white_pin = 5
+
+led_red_pin = 26
+led_green_pin = 19
+led_blue_pin = 13
+led_orange_pin = 6
+led_white_pin = 5
+
+btn_red_flag = 0
+btn_green_flag = 0
+btn_blue_flag = 0
+btn_orange_flag = 0
+btn_white_flag = 0
+
+rGPIO.setup(btn_red_pin, rGPIO.IN, pull_up_down=rGPIO.PUD_UP)
+rGPIO.setup(btn_green_pin, rGPIO.IN, pull_up_down=rGPIO.PUD_UP)
+rGPIO.setup(btn_blue_pin, rGPIO.IN, pull_up_down=rGPIO.PUD_UP)
+rGPIO.setup(btn_orange_pin, rGPIO.IN, pull_up_down=rGPIO.PUD_UP)
+rGPIO.setup(btn_white_pin, rGPIO.IN, pull_up_down=rGPIO.PUD_UP)
+
+rGPIO.setup(btn_red_pin,rGPIO.OUT)
+rGPIO.setup(led_green_pin,rGPIO.OUT)
+rGPIO.setup(led_blue_pin,rGPIO.OUT)
+rGPIO.setup(led_orange_pin,rGPIO.OUT)
+rGPIO.setup(led_white_pin,rGPIO.OUT)
+
+def btn_Callback(button_pin):
+    print button_pin
+    if button_pin == btn_red_pin:
+        if btn_red_flag == 0:
+            GPIO.output(btn_red_pin,GPIO.HIGH)
+        else:
+            GPIO.output(btn_red_pin,GPIO.LOW)
+    elif button_pin == btn_green_pin:
+        if btn_green_flag == 0:
+            GPIO.output(btn_green_pin,GPIO.HIGH)
+        else:
+            GPIO.output(btn_green_pin,GPIO.LOW)
+    elif button_pin == btn_blue_pin:
+        if btn_blue_flag == 0:
+            GPIO.output(btn_blue_pin,GPIO.HIGH)
+        else:
+            GPIO.output(btn_blue_pin,GPIO.LOW)    
+    elif button_pin == btn_orange_pin:
+        if btn_orange_flag == 0:
+            GPIO.output(btn_orange_pin,GPIO.HIGH)
+        else:
+            GPIO.output(btn_orange_pin,GPIO.LOW)    
+    elif button_pin == btn_white_pin:
+        if btn_white_flag == 0:
+            GPIO.output(btn_white_pin,GPIO.HIGH)
+        else:
+            GPIO.output(btn_white_pin,GPIO.LOW)
+
+
+rGPIO.add_event_detect(btn_red_pin, rGPIO.RISING, callback=btn_Callback, bouncetime=300)
+rGPIO.add_event_detect(btn_green_pin, rGPIO.RISING, callback=btn_Callback, bouncetime=300)
+rGPIO.add_event_detect(btn_blue_pin, rGPIO.RISING, callback=btn_Callback, bouncetime=300)
+rGPIO.add_event_detect(btn_orange_pin, rGPIO.RISING, callback=btn_Callback, bouncetime=300)
+rGPIO.add_event_detect(btn_white_pin, rGPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+# Main loop
+while True:
+    a = 1
