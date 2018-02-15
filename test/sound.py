18dd2111f49140b9478326b9014572d04ca070d1
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-15 15:45:15 +0000

    Update sound.py

--- test/sound.py
+++ test/sound.py
@@ -8,10 +8,10 @@ import RPi.GPIO as GPIO
 import pygame
 
 
-def playSound():
-    pygame.mixer.init()
-    pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
-    pygame.mixer.music.play()
+#def playSound():
+    #pygame.mixer.init()
+    #pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
+    #pygame.mixer.music.play()
     
 
 
@@ -20,9 +20,13 @@ try:
     #subprocess.Popen(['mpg123', '/home/pi/Development/DiscoCave/audio/vegimal.mp3'])
     #subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/veg.mp3 &')
     
-    t1 = threading.Thread(target=playSound)
-    t1.start()
-            
+    #t1 = threading.Thread(target=playSound)
+    #t1.start()
+    
+    
+    pygame.mixer.init()
+    pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
+    pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         print(".")
     
