582f8098ec64baadc358bf0ac7534baa481da869
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-15 14:32:29 +0000

    Update sound.py

--- test/sound.py
+++ test/sound.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 try:
-    #os.system('mpg123 -q /home/pi/Development/DiscoCave/audio/vegimal.mp3 &')
-    subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/veg.mp3 &')
+    subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/vegimal.mp3 &')
+    #subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/veg.mp3 &')
 except:
     print("Error")
