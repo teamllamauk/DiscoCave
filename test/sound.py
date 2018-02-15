c7ccc4d422d7c438bd8255f0970c139cc42f445d
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-15 15:49:27 +0000

    Update sound.py

--- test/sound.py
+++ test/sound.py
@@ -8,10 +8,15 @@ import RPi.GPIO as GPIO
 import pygame
 
 
-#def playSound():
-    #pygame.mixer.init()
-    #pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
-    #pygame.mixer.music.play()
+def playSound():
+    print("Start")
+    pygame.mixer.init()
+    pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        print(".")
+    
+    print("Finished")
     
 
 
@@ -20,17 +25,17 @@ try:
     #subprocess.Popen(['mpg123', '/home/pi/Development/DiscoCave/audio/vegimal.mp3'])
     #subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/veg.mp3 &')
     
-    #t1 = threading.Thread(target=playSound)
-    #t1.start()
+    t1 = threading.Thread(target=playSound)
+    t1.start()
     
     
-    pygame.mixer.init()
-    pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        print(".")
+    #pygame.mixer.init()
+    #pygame.mixer.music.load("/home/pi/Development/DiscoCave/audio/vegimal.mp3")
+    #pygame.mixer.music.play()
+    #while pygame.mixer.music.get_busy() == True:
+    #    print(".")
     
-    print("Finished")
+    #print("Finished")
     
 except:
     print("Unexpected error:", sys.exc_info()[0])
