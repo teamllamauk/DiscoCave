d648472346e7db741a670d2ba9e39a3cfd401818
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-15 14:48:03 +0000

    Update sound.py

--- test/sound.py
+++ test/sound.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-import os
+#import os
 import sys
+from pygame import mixer
 
 try:
     #subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/vegimal.mp3 &')
-    subprocess.Popen(['mpg123', '/home/pi/Development/DiscoCave/audio/vegimal.mp3'])
+    #subprocess.Popen(['mpg123', '/home/pi/Development/DiscoCave/audio/vegimal.mp3'])
     #subprocess.call('mpg123 -q /home/pi/Development/DiscoCave/audio/veg.mp3 &')
+    
+    mixer.init()
+    mixer.music.load('/home/pi/Development/DiscoCave/audio/vegimal.mp3')
+    mixer.music.play()
 except:
     print("Unexpected error:", sys.exc_info()[0])
