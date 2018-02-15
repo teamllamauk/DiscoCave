fe1308f58601c68562492b5fb23ff7f419fa085a
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-15 14:27:26 +0000

    Update sound.py

--- test/sound.py
+++ test/sound.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
-os.system('mpg123 -q /home/pi/Development/DiscoCave/audio/vegimal.mp3 &')
+try:
+    #os.system('mpg123 -q /home/pi/Development/DiscoCave/audio/vegimal.mp3 &')
+    os.system('mpg123 -q /home/pi/Development/DiscoCave/audio/veg.mp3 &')
+else:
+    print ("Error")
