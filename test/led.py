ff076490b14b83d442b65b5a297abf9fb28e6710
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-23 14:31:01 +0000

    Update led.py

--- test/led.py
+++ test/led.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import colorsys
 
 sys.path.insert(0, '/home/pi/Development/APA102_Pi')
 import apa102
@@ -11,27 +12,41 @@ strip = apa102.APA102(num_led=60, global_brightness=5, mosi = 10, sclk = 11, ord
 
 strip.clear_strip()
 
-def solidColour(ledColour):
+def convertHSVtoRGB(hsvColour)
+    rgbColour = colorsys.hsv_to_rgb(hsvColour,100,100)
+    print("HSV: ", hsvColour)
+    print("RGB: ", rgbColour)
+    
+def solidColour(ledHSVColour):
+    ledRGBColour = convertHSVtoRGB(ledHSVColour)
     strip.clear_strip()
     for x in range(0, 60):
-        strip.set_pixel_rgb(x, ledColour) 
-    strip.show()
+        strip.set_pixel_rgb(x, ledRGBColour) 
+    #strip.show()
    
+
+
+count = 0
+while count < 360:
+    solidColour(count)
+    time.sleep(2)
+    count = count + 10
     
-solidColour(0xFF0000) #Red
-time.sleep(2)
-solidColour(0xFFA500) #Orange
-time.sleep(2)
-solidColour(0xFFFF00) #Yellow
-time.sleep(2)
-solidColour(0x00FF00) #Green
-time.sleep(2)
-solidColour(0x0000FF) #Blue
-time.sleep(2)
-solidColour(0x4B0082) #Indigo
-time.sleep(2)
-solidColour(0xEE82EE) #Violet
-time.sleep(2)
+
+#solidColour(0xFF0000) #Red
+#time.sleep(2)
+#solidColour(0xFFA500) #Orange
+#time.sleep(2)
+#solidColour(0xFFFF00) #Yellow
+#time.sleep(2)
+#solidColour(0x00FF00) #Green
+#time.sleep(2)
+#solidColour(0x0000FF) #Blue
+#time.sleep(2)
+#solidColour(0x4B0082) #Indigo
+#time.sleep(2)
+#solidColour(0xEE82EE) #Violet
+#time.sleep(2)
 
 strip.clear_strip()
 strip.cleanup()
