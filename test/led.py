90753d1371fd6d2a57198ebb760324332ca807b5
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-23 15:29:16 +0000

    Update led.py

--- test/led.py
+++ test/led.py
@@ -7,14 +7,14 @@ import colorsys
 sys.path.insert(0, '/home/pi/Development/APA102_Pi')
 import apa102
 
-#strip = apa102.APA102(num_led=60, global_brightness=20, mosi = 10, sclk = 11, order='rgb')
-strip = apa102.APA102(num_led=60, global_brightness=5, mosi = 10, sclk = 11, order='rgb')
+strip = apa102.APA102(num_led=60, global_brightness=20, mosi = 10, sclk = 11, order='rgb')
+#strip = apa102.APA102(num_led=60, global_brightness=5, mosi = 10, sclk = 11, order='rgb')
 
 strip.clear_strip()
 
 def convertHSVtoRGB(hsvColour):
     rgbColour = colorsys.hsv_to_rgb(hsvColour/360,1,1)
-    print("HSV: ", hsvColour)    
+    #print("HSV: ", hsvColour)
         
     hR = int(rgbColour[0] * 256)
     if hR == 256: hR = 255    
@@ -25,19 +25,17 @@ def convertHSVtoRGB(hsvColour):
     hB = int(rgbColour[2] * 256)
     if hB == 256: hB = 255
     
-    rgbIntColour = (hR, hG, hB)
-    
+    rgbIntColour = (hR, hG, hB)    
     rgbHexColour = '0x%02x%02x%02x' % rgbIntColour
     
-    print(rgbHexColour)
+    #print(rgbHexColour)
     return int(rgbHexColour, 16)
 
     
 def solidColour(ledHSVColour):
     ledRGBColour = convertHSVtoRGB(ledHSVColour)
     strip.clear_strip()
-    #for x in range(0, 60):
-    for x in range(0, 1):
+    for x in range(0, 60):
         strip.set_pixel_rgb(x, ledRGBColour)
     strip.show()
    
@@ -46,8 +44,8 @@ def solidColour(ledHSVColour):
 count = 0
 while count < 360:
     solidColour(count)
-    time.sleep(1)
-    count = count + 10
+    time.sleep(0.5)
+    count = count + 30
     
 
 #solidColour(0xFF0000) #Red
