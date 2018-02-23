1e2f75d1aae4d9b1953a8ff61154990cf5c2a7a0
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-23 14:53:10 +0000

    Update led.py

--- test/led.py
+++ test/led.py
@@ -21,13 +21,13 @@ def convertHSVtoRGB(hsvColour):
     print("B: ", rgbColour[2])    
     
     hR = rgbColour[0] * 256
-    if hR = 256: hR = 255
+    if hR == 256: hR = 255
         
     hG = rgbColour[0] * 256
-    if hG = 256: hG = 255
+    if hG == 256: hG = 255
     
     hB = rgbColour[0] * 256
-    if hB = 256: hB = 255
+    if hB == 256: hB = 255
         
     print("Rx: ", hR)
     print("Gx: ", hG)
