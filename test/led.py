f890ef34241da5bbc5646a406cd2a895f6e0cca1
teamllamauk <teamllamauk@users.noreply.github.com>
2018-02-26 11:59:07 +0000

    Update led.py

--- test/led.py
+++ test/led.py
@@ -16,7 +16,7 @@ global availableColours
 global selectedColourPos
 
 availableColours = (0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330)
-selectedColourPos = 11
+selectedColourPos = 0
 
 def convertHSVtoRGB(hsvColour):
     rgbColour = colorsys.hsv_to_rgb(hsvColour/360,1,1)
@@ -53,55 +53,78 @@ def solidColour(ledHSVColour):
             
     ledRGBColour = convertHSVtoRGB(ledHSVColour)
     
-    strip.clear_strip()
+    #strip.clear_strip()
     for x in range(0, 60):
         strip.set_pixel_rgb(x, ledRGBColour)        
     strip.show()
 
 
-def slowRainbow():
-    count = 0
-    while count < 360:
-        solidColour(count)
-        time.sleep(1)
-        count = count + 30
-
-
-def fastRainbow():
-    count = 0
-    while count < 360:
-        solidColour(count)
-        time.sleep(0.1)
-        count = count + 30
+def rainbow(delay):
+    global availableColours
+    global selectedColourPos
+    
+    while True:
+        solidColour(availableColours[selectedColourPos])
+        selectedColourPos = selectedColourPos + 1
+        if selectedColourPos > 11: selectedColourPos = 0
+        time.sleep(delay)
 
 
 def rotateLEDs(delay):
     global availableColours
     global selectedColourPos    
-        
-    ledHSVColour = availableColours[selectedColourPos]    
-    ledRGBColour = convertHSVtoRGB(ledHSVColour)
     
-    count = 0
-    while count < 4:
+    while True:
         for x in range(0, 60):
-            strip.clear_strip()
-        
             ledOne = x
             ledTwo = x + 1
-            ledThree = x + 2
-        
-            if ledTwo == 60: ledTwo = 0
-            if ledThree == 60: ledThree = 0
-            if ledThree == 61: ledThree = 1
-        
+            ledThree = x + 2            
+            
+            ledHSVColour = availableColours[selectedColourPos]    
+            ledRGBColour = convertHSVtoRGB(ledHSVColour)
+            
+            strip.clear_strip()
             strip.set_pixel_rgb(ledOne, ledRGBColour, 5)
-            strip.set_pixel_rgb(ledTwo, ledRGBColour)
-            strip.set_pixel_rgb(ledThree, ledRGBColour, 5)
+            if ledTwo < 61: strip.set_pixel_rgb(ledTwo, ledRGBColour)
+            if ledThree < 61: strip.set_pixel_rgb(ledThree, ledRGBColour, 5)
             strip.show()
             time.sleep(delay)
-        count= count + 1        
-    strip.clear_strip()
+
+
+def bounceLEDs(delay):
+    global availableColours
+    global selectedColourPos
+    
+    while True:
+        for x in range(0, 60):
+            ledOne = x
+            ledTwo = x + 1
+            ledThree = x + 2            
+            
+            ledHSVColour = availableColours[selectedColourPos]    
+            ledRGBColour = convertHSVtoRGB(ledHSVColour)
+            
+            strip.clear_strip()
+            strip.set_pixel_rgb(ledOne, ledRGBColour, 5)
+            if ledTwo < 61: strip.set_pixel_rgb(ledTwo, ledRGBColour)
+            if ledThree < 61: strip.set_pixel_rgb(ledThree, ledRGBColour, 5)
+            strip.show()
+            time.sleep(delay)
+            
+        for x in range(60, 0, -1):
+            ledOne = x
+            ledTwo = x - 1
+            ledThree = x - 2            
+            
+            ledHSVColour = availableColours[selectedColourPos]    
+            ledRGBColour = convertHSVtoRGB(ledHSVColour)
+            
+            strip.clear_strip()
+            strip.set_pixel_rgb(ledOne, ledRGBColour, 5)
+            if ledTwo < 0: strip.set_pixel_rgb(ledTwo, ledRGBColour)
+            if ledThree < 0: strip.set_pixel_rgb(ledThree, ledRGBColour, 5)
+            strip.show()
+            time.sleep(delay)
 
 
 def simButton():
@@ -119,13 +142,13 @@ strip.clear_strip()
 time.sleep(2)
 
 print("Fast Rainbow")
-fastRainbow()
+rainbow(1)
 
 strip.clear_strip()
 time.sleep(2)
 
 print("Slow Rainbow")
-slowRainbow()
+rainbow(0.1)
 
 strip.clear_strip()
 time.sleep(2)
