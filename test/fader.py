0a8f80572b62929a219f5995385ae5ebe9499553
teamllamauk <teamllamauk@users.noreply.github.com>
2018-04-04 11:03:26 +0100

    Create fader.py

---
+++ test/fader.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Imports
+import sys
+import time
+import threading
+import colorsys
+import RPi.GPIO as rGPIO
+
+sys.path.insert(0, '/home/pi/Development/APA102_Pi')
+import apa102
+
+strip = apa102.APA102(num_led=60, global_brightness=20, mosi = 10, sclk = 11, order='rgb')
+
+strip.clear_strip()
+
+def convertHSVtoRGB(hsvColour):
+    rgbColour = colorsys.hsv_to_rgb(hsvColour/360,1,1)
+    #print("HSV: ", hsvColour)
+        
+    hR = int(rgbColour[0] * 256)
+    if hR == 256: hR = 255    
+        
+    hG = int(rgbColour[1] * 256)
+    if hG == 256: hG = 255
+    
+    hB = int(rgbColour[2] * 256)
+    if hB == 256: hB = 255
+    
+    rgbIntColour = (hR, hG, hB)    
+    rgbHexColour = '0x%02x%02x%02x' % rgbIntColour
+    
+    #print(rgbHexColour)
+    return int(rgbHexColour, 16)
+
+
+ledRGBColour = convertHSVtoRGB(60)
+
+while True:
+
+    for fadeBrightness in range(0, 20):
+        for ledID in range(0, 60):
+            strip.set_pixel_rgb(ledID, ledRGBColour, fadeBrightness)        
+        strip.show()
+        time.sleep(0.1)
+        
+    for fadeBrightness in range(20, 0, -1):
+        for ledID in range(0, 60):
+            strip.set_pixel_rgb(ledID, ledRGBColour, fadeBrightness)        
+        strip.show()
+        time.sleep(0.1)
