4bfdd1be7da5c8100866d438a561a9081069e60a
teamllamauk <teamllamauk@users.noreply.github.com>
2018-04-05 09:55:19 +0100

    Update timeofday.py

--- test/timeofday.py
+++ test/timeofday.py
@@ -12,7 +12,9 @@ timeNowHHmm = timeNow.strftime("%H:%M")
 print(timeNowHHmm)
 
 timeOn = datetime.datetime.strptime("07:00", "%H:%M")
+timeOnHHmm = timeOn.strftime("%H:%M")
 print(timeOn)
+print(timeOnHHmm)
 
 timeOff = datetime.datetime.strptime("19:00", "%H:%M")
 print(timeOff)
