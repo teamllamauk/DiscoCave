8498f2a7a0255be15c6cd591d1d50b643e3c1fb5
teamllamauk <teamllamauk@users.noreply.github.com>
2018-04-05 10:26:07 +0100

    Update timeofday.py

--- test/timeofday.py
+++ test/timeofday.py
@@ -4,20 +4,20 @@
 import time
 import datetime
 
+def checkTime()
+    timeNow = datetime.datetime.now()
+    timeNowHHmm = timeNow.strftime("%H:%M")
+    
+    timeOn = datetime.datetime.strptime("07:00", "%H:%M")
+    timeOnHHmm = timeOn.strftime("%H:%M")
+    
+    timeOff = datetime.datetime.strptime("19:00", "%H:%M")
+    timeOffHHmm = timeOff.strftime("%H:%M")
+    
+    if timeNowHHmm > timeOnHHmm and timeNowHHmm < timeOffHHmm:
+        return True
+    else
+        return False
 
-timeNow = datetime.datetime.now()
-print(timeNow)
-
-timeNowHHmm = timeNow.strftime("%H:%M")
-print(timeNowHHmm)
-
-timeOn = datetime.datetime.strptime("07:00", "%H:%M")
-timeOnHHmm = timeOn.strftime("%H:%M")
-print(timeOnHHmm)
-
-timeOff = datetime.datetime.strptime("19:00", "%H:%M")
-timeOffHHmm = timeOff.strftime("%H:%M")
-print(timeOffHHmm)
-
-if timeNowHHmm > timeOnHHmm and timeNowHHmm < timeOffHHmm:
-    print("OK")
+    
+print(checkTime())
