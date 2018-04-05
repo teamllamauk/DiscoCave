9ede502a1fc184916451b907995e718776b24f82
teamllamauk <teamllamauk@users.noreply.github.com>
2018-04-05 10:34:26 +0100

    Update timeofday.py

--- test/timeofday.py
+++ test/timeofday.py
@@ -5,13 +5,19 @@ import time
 import datetime
 
 def checkTime():
+    #wakeUp = "07:00"
+    #goToBed = "19:00"
+    
+    wakeUp = "10:36"
+    goToBed = "10:38"
+    
     timeNow = datetime.datetime.now()
     timeNowHHmm = timeNow.strftime("%H:%M")
     
-    timeOn = datetime.datetime.strptime("07:00", "%H:%M")
+    timeOn = datetime.datetime.strptime(wakeUp, "%H:%M")
     timeOnHHmm = timeOn.strftime("%H:%M")
     
-    timeOff = datetime.datetime.strptime("19:00", "%H:%M")
+    timeOff = datetime.datetime.strptime(goToBed, "%H:%M")
     timeOffHHmm = timeOff.strftime("%H:%M")
     
     if timeNowHHmm > timeOnHHmm and timeNowHHmm < timeOffHHmm:
@@ -19,5 +25,8 @@ def checkTime():
     else:
         return False
 
+while True:
     
-print(checkTime())
+    timeNow = datetime.datetime.now()
+    timeNowHHmm = timeNow.strftime("%H:%M")
+    print(checkTime(), " - ", timeNowHHmm)
