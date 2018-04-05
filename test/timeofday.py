5634ac3223dd8a348a35581ab902c7392503be6c
teamllamauk <teamllamauk@users.noreply.github.com>
2018-04-05 09:48:22 +0100

    Update timeofday.py

--- test/timeofday.py
+++ test/timeofday.py
@@ -8,11 +8,11 @@ import datetime
 timeNow = datetime.datetime.now()
 print(timeNow)
 
-timeHHmm = timeNow.strftime("%H:%M")
-#timeHHmm = datetime.datetime.strptime(timeNow, "%H:%M")
-print(timeHHmm)
+timeNowHHmm = timeNow.strftime("%H:%M")
+print(timeNowHHmm)
 
+timeOn = datetime.datetime.strptime("07:00", "%H:%M")
+print(timeOn)
 
-
-#def timeDiff():
-    
+timeOff = datetime.datetime.strptime("19:00", "%H:%M")
+print(timeOff)
